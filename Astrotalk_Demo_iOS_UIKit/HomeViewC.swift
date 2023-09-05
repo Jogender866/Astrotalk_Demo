f1c9e2653c017da824860388ed50ad4547e4bdd2
jogender866 <jogendersingh866@gmail.com>
2023-09-05 14:42:19 +0530

    Initial Commit With Swiftlint and Initial Setup

---
+++ Astrotalk_Demo_iOS_UIKit/HomeViewC.swift
@@ -0,0 +1,24 @@
+//
+//  HomeViewC.swift
+//  Astrotalk_Demo_iOS_UIKit
+//
+//  Created by Jogender Singh on 05/09/23.
+//
+
+import UIKit
+
+class HomeViewC: UIViewController {
+    // MARK: - IBOUTLETS
+    // MARK: - PROPERTIES
+    // MARK: - VIEW LIFE CYCLE METHODS
+    // TODO: VIEW DID LOAD
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // TODO: DEINIT
+    deinit {
+        print("HomeViewC DEINIT...!")
+    }
+    // MARK: - ACTIONS
+    
+}
--- Astrotalk_Demo_iOS_UIKit/SystemFiles/AppDelegate.swift
+++ Astrotalk_Demo_iOS_UIKit/SystemFiles/AppDelegate.swift
@@ -11,20 +11,21 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
-
+    // MARK: - APP LIFE CYCLE METHODS
+    // TODO: Did Finish Launching With Options
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         return true
     }
 
-    // MARK: UISceneSession Lifecycle
-
+    // MARK: UISCENESEESSION LIFECYCLE
+    // TODO: Configuration For Connecting
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    // TODO:   Did Discard Scene Sessions
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
