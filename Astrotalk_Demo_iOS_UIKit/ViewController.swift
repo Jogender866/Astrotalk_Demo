a312639800d1f263c39b30edc8c82d49749903e3
jogender866 <jogendersingh866@gmail.com>
2023-09-05 14:24:12 +0530

    Initial Commit

---
+++ Astrotalk_Demo_iOS_UIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Astrotalk_Demo_iOS_UIKit
+//
+//  Created by Jogender Singh on 05/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
