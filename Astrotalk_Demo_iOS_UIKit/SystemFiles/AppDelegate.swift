e624a253db4d0b81f50818bd8248647a83aeab90
jogender866 <jogendersingh866@gmail.com>
2023-09-14 13:57:12 +0530

    added Crashlytics

--- Astrotalk_Demo_iOS_UIKit/SystemFiles/AppDelegate.swift
+++ Astrotalk_Demo_iOS_UIKit/SystemFiles/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseCore
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // TODO: Did Finish Launching With Options
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return true
     }
 
