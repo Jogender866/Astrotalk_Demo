a7acc6f83724cea48543e5e8d94c0b0905511212
jogender866 <jogendersingh866@gmail.com>
2023-09-14 16:01:43 +0530

    Added Analytics

--- Astrotalk_Demo_iOS_UIKit/HomeViewC.swift
+++ Astrotalk_Demo_iOS_UIKit/HomeViewC.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAnalytics
 
 class HomeViewC: UIViewController {
     // MARK: - IBOUTLETS
@@ -14,20 +16,11 @@ class HomeViewC: UIViewController {
     // TODO: VIEW DID LOAD
     override func viewDidLoad() {
         super.viewDidLoad()
-        let button = UIButton(type: .roundedRect)
-              button.frame = CGRect(x: 20, y: 50, width: 100, height: 30)
-              button.setTitle("Test Crash", for: [])
-              button.addTarget(self, action: #selector(self.crashButtonTapped(_:)), for: .touchUpInside)
-              view.addSubview(button)
     }
     // TODO: DEINIT
     deinit {
         print("HomeViewC DEINIT...!")
     }
     // MARK: - ACTIONS
-    @IBAction func crashButtonTapped(_ sender: AnyObject) {
-         let numbers = [0]
-         let _ = numbers[1]
-     }
     
 }
